0774334cdc8e94e17dcec7850b10ac36f36d6420
ichiho <ichibon0@gmail.com>
2015-09-01 21:25:10 +0900

    画像をパケットに変換する機能を実装

--- src/send.cpp
+++ src/send.cpp
@@ -7,18 +7,21 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <net/if.h>
-#include <net/ethernet.h>
-#include <netinet/in.h>
+#include "lib.h"
 
 void mtov(cv::Mat, std::vector< std::vector< cv::Vec3b> >&);
 void vtom(std::vector< std::vector< cv::Vec3b> >, cv::Mat&);
 
 int main(int argc, char *argv[])
 {
+  int fd, size;
+
   std::vector< std::vector<cv::Vec3b> > vec;
+  std::vector<unsigned char> buffer{
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, //Destination MAC Address
+    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, //Source MAC Address
+    0x15, 0x15 //EtherType
+  };
 
   cv::VideoCapture cam(atoi(argv[2]));
   cam.set(CV_CAP_PROP_FRAME_WIDTH, atoi(argv[3]));
@@ -37,6 +40,24 @@ int main(int argc, char *argv[])
   std::cout << img.cols << " " << img.rows << std::endl;
 
   mtov(img, vec);
+
+  int height = vec.size(), width = vec[0].size();
+
+  buffer.push_back(height>>8); buffer.push_back(height);
+  buffer.push_back(width>>8); buffer.push_back(width);
+  buffer.push_back(img.channels());
+
+  for(int i = 0; i < height; i++) {
+    for(int j = 0; j < width; j++) {
+      for(int k = 0; k < 3; k++) {
+	//printf("%d ", vec[i][j][k]);
+	buffer.push_back(vec[i][j][k]);
+      } //printf("\n");
+    }
+  }
+  //  for(int i = 0; i < 10000; i++) buffer.push_back(0x00);
+
+  for(auto v: buffer) printf("%x ", v); std::cout << std::endl;
 
   // for(int i = 0; i < vec.size(); i++) {
   //   for(int j = 0; j < vec[i].size(); j++) {
@@ -58,9 +79,14 @@ int main(int argc, char *argv[])
   // std::cout << tmp << std::endl;
   // tmp.reshape(480, 640);
 
-  cv::Mat tmp;
-  vtom(vec, tmp);
-  cv::imshow("hoge", tmp); cv::waitKey(5000);
+  //cv::Mat tmp;
+  //vtom(vec, tmp);
+  //cv::imshow("hoge", tmp); cv::waitKey(5000);
+
+  fd = pkthandler.open_send(argv[1], 0);
+  size = buffer.size();
+
+  if(size > 0) pkthandler.send(fd, (char *)&buffer[0], size);
 
   return 0;
 }
